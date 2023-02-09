6527e2f7b2b18f50d6cd550c0b2f148567cc1640
Percy Ku <percyku19@gmail.com>
2023-02-09 20:23:58 +0800

    Initial commit

---
+++ src/main/java/com/percyku/springbootmallprarice/SpringbootMallPrariceApplication.java
@@ -0,0 +1,13 @@
+package com.percyku.springbootmallprarice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallPrariceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMallPrariceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/percyku/springbootmallprarice/SpringbootMallPrariceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.percyku.springbootmallprarice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallPrariceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
