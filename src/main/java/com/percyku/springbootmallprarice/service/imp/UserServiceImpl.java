30da362ae9464b6a1aff69cbed2f9e4e67e6bb66
Percy Ku <percyku19@gmail.com>
2023-02-11 17:52:18 +0800

    Account Function - Use MD5 to encrypt

--- src/main/java/com/percyku/springbootmallprarice/service/imp/UserServiceImpl.java
+++ src/main/java/com/percyku/springbootmallprarice/service/imp/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -35,6 +36,10 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        // using MD5 product password Hash
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         //create user account
         return userDao.createUser(userRegisterRequest);
     }
@@ -49,8 +54,12 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        // using MD5 product password Hash
+        String hashedPassword =  DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
         //check password is correct or not
-        if(user.getPassword().equals(userLoginRequest.getPassword())){
+        //if(user.getPassword().equals(userLoginRequest.getPassword())){
+        if(user.getPassword().equals(hashedPassword)){
             return user;
         }
         else{
