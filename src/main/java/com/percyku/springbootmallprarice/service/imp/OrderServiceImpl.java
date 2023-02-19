782b5944b90d8fee5830374bf14c6c38cd4402d0
Percy.Ku <percyku19@gmail.com>
2023-02-19 10:17:56 +0800

    Order Function - check product exist or not, and check product is enough or not

--- src/main/java/com/percyku/springbootmallprarice/service/imp/OrderServiceImpl.java
+++ src/main/java/com/percyku/springbootmallprarice/service/imp/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package com.percyku.springbootmallprarice.service.imp;
 
 import com.percyku.springbootmallprarice.dao.OrderDao;
 import com.percyku.springbootmallprarice.dao.ProductDao;
+import com.percyku.springbootmallprarice.dao.UserDao;
 import com.percyku.springbootmallprarice.dto.BuyItem;
 import com.percyku.springbootmallprarice.dto.CreateOrderRequest;
 import com.percyku.springbootmallprarice.model.Order;
@@ -11,6 +12,8 @@ import com.percyku.springbootmallprarice.model.Product;
 import com.percyku.springbootmallprarice.model.User;
 import com.percyku.springbootmallprarice.service.OrderService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -23,7 +26,7 @@ import java.util.List;
 
 @Component
 public class OrderServiceImpl implements OrderService {
-
+    private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
 
 
     @Autowired
@@ -31,6 +34,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public Order getOrderById(Integer orderId) {
@@ -45,6 +51,14 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
 
+        //check user exist or not
+        User user = userDao.getUserById(userId);
+
+        if(user == null){
+            log.warn("This userId {} is not exist",userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
 
         int totalAmount = 0;
         List<OrderItem> orderItemList =new ArrayList<>();
@@ -52,6 +66,19 @@ public class OrderServiceImpl implements OrderService {
         for(BuyItem buyItem : createOrderRequest.getBuyItemList()){
             Product product = productDao.getProductById(buyItem.getProductId());
 
+
+            //check product exist or not, and check product is enough or not
+            if(product == null){
+                log.warn("商品 {} 不存在",buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }else if(product.getStock() < buyItem.getQuantity()){
+                log.warn("商品 {} 庫存數量不足，無法購買。剩餘庫存 {} ，欲購買數量 {}",
+                        buyItem.getProductId(),product.getStock(), buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            //扣除商品庫存
+            productDao.updateStock(product.getProductId(),product.getStock() - buyItem.getQuantity());
 
 
             //total price
